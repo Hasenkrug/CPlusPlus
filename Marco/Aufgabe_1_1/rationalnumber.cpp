f359c05c290f323aa2ca4da2f82a6d5d14229919
Marco W <marco.d.witt@gmx.de>
2013-04-08 16:35:43 +0200

    ordnerSTruktur angelegt

---
+++ Marco/Aufgabe_1_1/rationalnumber.cpp
@@ -0,0 +1,5 @@
+#include "rationalnumber.h"
+
+Rationalnumber::Rationalnumber()
+{
+}
---
+++ Marco/Aufgabe_1_1/rationalnumber.h
@@ -0,0 +1,10 @@
+#ifndef RATIONALNUMBER_H
+#define RATIONALNUMBER_H
+
+class Rationalnumber
+{
+public:
+    Rationalnumber();
+};
+
+#endif // RATIONALNUMBER_H
