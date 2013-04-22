982083fcf7a4307b4eaacc061cf6c29b9905e423
Marco W <marco.d.witt@gmx.de>
2013-04-22 16:34:43 +0200

    rncSum/rncAverage

--- Marco/Aufgabe_1_1/rationalnumbercollection.cpp
+++ Marco/Aufgabe_1_1/rationalnumbercollection.cpp
@@ -1,15 +1,80 @@
 #include "rationalnumbercollection.h"
 #include "rationalnumber.h"
-#include <stdio.h>
 
-void rncInit(RationalNumberCollection* rnc){
-    rnc[1000];
-    //rnc->rationalnumber;
-    printf("djsknfksjn: %i", rnc->counter);
+
+bool rncInit(RationalNumberCollection* c) {
+
+    RationalNumber rn = { 0, 1 };
+
+    c->size = 1000;
+    c->nfi = 0;
+    c->rnSum = rn;
+
+    for(int i = 0; i < 1000; i++) {
+        c->collection[i].rn = rn;
+        c->collection[i].count = 0;
+    }
+
+    return true;
 }
-void rncAdd(RationalNumberCollection* rnc, RationalNumber rn){
-    rnc[rnc->counter]->rationalnumber = rn;
-    rnc[rnc->counter]->counter = rnc[rnc->counter]->counter+1;
-    printf("djsknfksjn: %i", rnc[rnc->counter]->rationalnumber.denominator);
+
+int rncFindPosition(RationalNumberCollection* c, RationalNumber n) {
+    for(int i = 0; i < c->size; i++) {
+        if(c->collection[i].count != 0) {
+            if(c->collection[i].rn.numerator == n.numerator) {
+                if(c->collection[i].rn.denominator == n.denominator) {
+                    return i;
+                }
+            }
+        }
+    }
+    return -1;
 }
+
+bool rncAdd(RationalNumberCollection* c, RationalNumber n) {
+    int pos = rncFindPosition(c,n);
+
+    if(pos != -1) {
+        c->collection[pos].count++;
+        c->rnSum = rnAdd(c->rnSum,c->collection[pos].rn);
+        return true;
+    } else if(c->nfi < c->size) {
+        c->collection[c->nfi].rn.numerator = n.numerator;
+        c->collection[c->nfi].rn.denominator = n.denominator;
+        c->collection[c->nfi].count++;
+        c->nfi++;
+        c->Sum = rnAdd(c->Sum,c->collection[c->collection[c->nfi].rn]);
+
+        return true;
+    }
+    return false;
+}
+
+bool rncRemove(RationalNumberCollection *c, RationalNumber n) {
+    int pos = rncFindPosition(c,n);
+
+    if(pos != -1 && c->collection[pos].count > 1) {
+        c->collection[pos].count--;
+        c->rnSum = rnSubtract(c->rnSum,c->collection[pos].rn);
+        return true;
+    } else if(pos != -1 && c->collection[pos].count == 1) {
+        c->collection[pos] = c->collection[c->nfi-1];
+        c->collection[c->nfi-1].count = 0;
+        c->nfi--;
+        c->Sum = rnSubtract(c->Sum,c->collection[c->collection[c->nfi].rn]);
+
+        return true;
+    }
+    return false;
+}
+
+
+RationalNumber rncSum(RationalNumberCollection* c){
+    return c->rnSum;
+}
+
+RationalNumber rncAverage(RationalNumberCollection* c){
+    return rnDivide(c->rncSum,rncTotalCount(c));
+}
+
 
