6941554558f5f539d61b3285f7b6df4a33e35d2a
Marco W <marco.d.witt@gmx.de>
2013-04-23 10:20:36 +0200

    gemergt

--- Marco/Aufgabe_1_1/rationalnumbercollection.cpp
+++ Marco/Aufgabe_1_1/rationalnumbercollection.cpp
@@ -1,7 +1,4 @@
-#include <stdio.h>
-
 #include "rationalnumbercollection.h"
-#include "rationalnumber.h"
 
 
 bool rncInit(RationalNumberCollection* c) {
@@ -34,25 +31,57 @@ int rncFindPosition(RationalNumberCollection* c, RationalNumber n) {
     return -1;
 }
 
+int rncFindIndex(RationalNumberCollection* c, RationalNumber n) {
+    int von = 0;
+    int bis = c->nfi-1;
+
+    while(von <= bis) {
+        int mitte = von + (bis-von) / 2;
+
+        if(rnLessThan(c->collection[mitte].rn,n)) {
+            von = mitte + 1;
+        } else if(rnLessThan(n,c->collection[mitte].rn)) {
+            bis = mitte - 1;
+        } else {
+            return mitte;
+        }
+    }
+    return von;
+}
+
 bool rncAdd(RationalNumberCollection* c, RationalNumber n) {
     int pos = rncFindPosition(c,n);
 
     if(pos != -1) {
         c->collection[pos].count++;
         RationalNumber summand = c->collection[pos].rn;
-        printf("jdsnfdj: %i %i", summand.numerator,summand.denominator);
         c->rnSum = rnAdd(c->rnSum,summand);
         c->totalCount++;
         return true;
     } else if(c->nfi < c->size) {
-        c->collection[c->nfi].rn.numerator = n.numerator;
-        c->collection[c->nfi].rn.denominator = n.denominator;
-        c->collection[c->nfi].count++;
-        RationalNumber summand = c->collection[c->nfi].rn;
+        RationalNumber summand = n;
         c->rnSum = rnAdd(c->rnSum,summand);
-        c->nfi++;
         c->totalCount++;
-        return true;
+        if(rncFindIndex(c,n) == c->nfi) {
+            c->collection[c->nfi].rn.numerator = n.numerator;
+            c->collection[c->nfi].rn.denominator = n.denominator;
+            c->collection[c->nfi].count++;
+
+            c->nfi++;
+        } else {
+            int tempIndex = c->nfi;
+            while(rncFindIndex(c,n) < tempIndex) {
+                CollectionElement tempInhalt = c->collection[tempIndex - 1];
+                c->collection[tempIndex] = tempInhalt;
+                tempIndex--;
+            }
+
+            int stelle = rncFindIndex(c,n);
+            c->collection[stelle].rn = n;
+            c->collection[stelle].count = 1;
+            c->nfi += 1;
+            return true;
+        }
     }
     return false;
 }
@@ -87,7 +116,6 @@ int rncCount(RationalNumberCollection *c, RationalNumber n) {
     return 0;
 }
 
-
 int rncTotalUniqueCount(RationalNumberCollection *c) {
     return c->nfi;
 }
@@ -104,5 +132,3 @@ RationalNumber rncAverage(RationalNumberCollection* c){
     RationalNumber divisor = {rncTotalCount(c),1};
     return rnDivide(c->rnSum, divisor);
 }
-
-
