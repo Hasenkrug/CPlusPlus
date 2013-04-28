124fff9a08495622bfa7541c69541a3a106ca702
nadine <nadinepusch@web.de>
2013-04-28 15:19:28 +0200

    documentiert part 1

--- ABGABEN/Aufgabe01/Aufgabe_1_1_und_1_2/rationalnumber.h
+++ ABGABEN/Aufgabe01/Aufgabe_1_1_und_1_2/rationalnumber.h
@@ -1,27 +1,36 @@
 #ifndef RATIONALNUMBER_H
 #define RATIONALNUMBER_H
 
-
+// this data type represents a rationalnumber with an int value for each component
 struct RationalNumber {
     int numerator, denominator;
 };
 
+//gets a rationalnumber and returns its greatest common divisor
 int eukl(RationalNumber n);
 
+//gets a rationalnumber and returns the reduced rationalnumber
 RationalNumber normalize(RationalNumber n);
 
+//returns true, if the rationalnumber is a valid number
 bool rnIsValid(RationalNumber n);
 
+//returns true, if a rationalnumber x is equals a rationalnumber y
 bool rnEqual(RationalNumber x, RationalNumber y);
 
+//returns true, if a rationalnumber x is less than a rationalnumber y
 bool rnLessThan(RationalNumber x, RationalNumber y);
 
+//adds two rationalnumbers and returns the result
 RationalNumber rnAdd(RationalNumber x, RationalNumber y);
 
+//subtracts two rationalnumbers and returns the result
 RationalNumber rnSubtract(RationalNumber x, RationalNumber y);
 
+//multiplies two rationalnumbers and returns the result
 RationalNumber rnMultiply(RationalNumber x, RationalNumber y);
 
+//divides two rationalnumbers and returns the result
 RationalNumber rnDivide(RationalNumber x, RationalNumber y);
 
 #endif // RATIONALNUMBER_H
--- ABGABEN/Aufgabe01/Aufgabe_1_1_und_1_2/rationalnumbercollection.h
+++ ABGABEN/Aufgabe01/Aufgabe_1_1_und_1_2/rationalnumbercollection.h
@@ -2,12 +2,17 @@
 #define RATIONALNUMBERCOLLECTION_H
 #include "rationalnumber.h"
 
-
+// this data type represents an element of a collection
+// it contains a rationalnumber and a counter
 struct CollectionElement {
     RationalNumber rn;
     int count;
 };
 
+// this data type represents a rationalnumber collection
+// it contains a collection of collectionelements, a value size,
+// a value representing the next free index (nfi), a value rnsum,
+// a value totalCount and a value uniqueCount
 struct RationalNumberCollection {
     CollectionElement collection[1000];
     int size;
@@ -17,20 +22,28 @@ struct RationalNumberCollection {
     int totalUniqueCount;
 };
 
+//gets a rationalnumbercollection by reference and initializes it
 bool rncInit(RationalNumberCollection* c);
 
+//gets a rationalnumbercollection by reference and adds a rationalnumber
 bool rncAdd(RationalNumberCollection* c, RationalNumber n);
 
+//gets a rationalnumbercollection by reference and removes a rationalnumber
 bool rncRemove(RationalNumberCollection* c, RationalNumber n);
 
+//gets a rationalnumber and returns the count of it, containing in the collection
 int rncCount(RationalNumberCollection* c, RationalNumber n);
 
+//returns the count of occupied elements in the collection
 int rncTotalUniqueCount(RationalNumberCollection* c);
 
+//returns the count of rationalnumbers in the collection
 int rncTotalCount(RationalNumberCollection* c);
 
+//returns the sum of all rationalnumbers in the collection
 RationalNumber rncSum(RationalNumberCollection* c);
 
+//returns the average of rationalnumbers in the collection
 RationalNumber rncAverage(RationalNumberCollection* c);
 
 #endif // RATIONALNUMBERCOLLECTION_H
