5768aabe6fca55a125e97da64e91138eb6772faa
Marco W <marco.d.witt@gmx.de>
2013-05-27 12:26:36 +0200

    bisschen weiter

--- Marco/aufgabe_2_1/mydate_map.cpp
+++ Marco/aufgabe_2_1/mydate_map.cpp
@@ -1,17 +1,66 @@
 #include "mydate_map.h"
 #include "mydate.h"
+#include "iostream"
+
+namespace MyDate {
+
+    size_t Map::size() const {
+        return m_size;
+    }
+
+    Map::Node* Map::getRootNode() {
+        return m_root;
+    }
 
 
-namespace MyDate
-{
-   size_t Map::Node::size() const{
-       return Map::m_size;
-   }
+    Map::Node* Map::Node::find(const Map::key_t &key) {
 
-   mapped_t& Map::Node::operator [](mapped_t[](const key_t& key){
+        if(this->m_pair.first == key){
+            return this;
+        }
+        if(this->m_pair.first < key){
+            find(this->m_right->m_pair.first);
+        }
+        if(this->m_pair.first > key){
+            find(this->m_left->m_pair.first);
+        }
+        return 0;
+    }
 
-       return
-   }
+    Map::Node* Map::Node::insert(const Map::key_t &key, const Map::mapped_t &value) {
+        Node *n;
+        if(this->find(key) == 0){
+            n = new Node(key,value,this);
+        }
+        return n;
+    }
+
+    Map::mapped_t& Map::operator[](const Map::key_t& key) {
+
+        if(find){
+            m_root->insert(key,m_root->m_pair.second);
+            return;
+        }
+        if(m_root->m_pair.first > key){
+            m_root[m_root->m_left->m_pair.first];
+        }
+        if(m_root->m_pair.first < key){
+            m_root[m_root->m_right->m_pair.first];
+        }
+        if(m_root->m_pair.first == key){
+
+            return m_root->m_pair.second;
+        }
+
+    }
+    bool Map::contains(const key_t& key){
+        if(m_root.find(key) == 0){
+            return false;
+        }
+        return true;
+    }
+
+    const Map::mapped_t& operator[](const Map::key_t& key){
+        if(m_root[m_root];
+    }
 }
-
-
--- Marco/aufgabe_2_1/mydate_map.h
+++ Marco/aufgabe_2_1/mydate_map.h
@@ -1,62 +1,47 @@
 #ifndef MYDATE_MAP_H
 #define MYDATE_MAP_H
-#include <mydate.h>
 
+#include "mydate.h"
+#include "iostream"
 
-namespace MyDate
-{
-    // associative map date --> string
-    class Map
-    {
-    public:
+namespace MyDate {
 
-        // aliases for key type and mapped type
-        typedef MyDate::Date key_t;
-        typedef std::string mapped_t;
-        Map()
-        {
-
-        }
-
-    protected:
-
-        // internal node type for binary search tree
-        class Node{
+    class Map {
 
         public:
-            std::pair<key_t, mapped_t> m_pair; // key/value pair
-            Node *m_up, *m_left, *m_right; // pointers to other nodes
+            // aliases for key type and mapped type
+            typedef MyDate::Date key_t;
+            typedef std::string mapped_t;
 
-            size_t size() const;//
+            Map():
+                m_root(0), m_size(0){}
+
+        protected:
+            // internal node type for binary search tree
+            class Node {
+
+                public:
+                    std::pair<key_t, mapped_t> m_pair; // key/value pair
+                    Node *m_up, *m_left, *m_right; // pointers to other nodes
+
+                    Node(const key_t& key, const mapped_t& value, Node* parent):
+                        m_pair(key, value), m_up(parent) {}
+
+                    Node* find(const key_t& key);
+                    Node* insert(const key_t& key, const mapped_t& value);
+            };
+
+            Node* m_root; // root node of search tree
+            size_t m_size; // number of elements in tree
+            const mapped_t M_NOT_IN_MAP; // return value if not in map
 
 
-            /*Node(const key_t& key, const mapped_t& value, Node* parent)
-
-            {
-                m_pair<key,value> ;
-                m_up = parent;
-            }*/
-
-            mapped_t& operator[](const key_t& key);
-            //Node* find(const key_t& key);
-            //Node* insert(const key_t& key,
-            //const mapped_t& value);
-
-            //bool contains(const key_t& key) const;
-
-            //const mapped_t& operator[](const key_t& key) const;
-        };
-
-        Node* m_root;// root node of search tree
-        size_t m_size;// number of elements in tree
-        const mapped_t M_NOT_IN_MAP; // return value if not in map
-
+            size_t size() const;
+            Node* getRootNode();
+            bool contains(const key_t& key);
+            mapped_t& operator[](const Map::key_t& key);
+            const mapped_t& ooperator(const key_t& key);
     };
 }
-class mydate_map
-{
-public:
-    mydate_map();
-};
 
 #endif // MYDATE_MAP_H
