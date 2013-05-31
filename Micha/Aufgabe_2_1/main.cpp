0d7e3795c3c36f2b1d56b405eda0fe37d13955a7
Hasenkrug <togomade@gmail.com>
2013-06-01 01:21:57 +0200

    chice ausgabe

--- Micha/Aufgabe_2_1/main.cpp
+++ Micha/Aufgabe_2_1/main.cpp
@@ -16,10 +16,10 @@ using namespace MyDate;
 
 int main() {
 
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "Map-Test-Area" << endl;
-    cout << "" << endl;
+    cout << "                                                         " << endl;
+    cout << "                                                         " << endl;
+    cout << "                         Map-Test-Area                   " << endl;
+    cout << "                                                         " << endl;
 
 
     MyDate::Date date1(1,5,2013);    
@@ -29,6 +29,7 @@ int main() {
     MyDate::Date date4(6,4,2013);
     MyDate::Date date5(5,5,2013);
     MyDate::Date date6(5,4,2013);
+    MyDate::Date date7(7,4,2013);
 
     Map map1;
 
@@ -44,34 +45,38 @@ int main() {
     map1[date4] = "der 06.04.2013";
     map1[date5] = "der 05.05.2013";
     map1[date6] = "der 05.04.2013";
+    map1[date7] = "der 07.04.2013";
 
+    assert(map1.size() == 7);
 
-    /*             Unsere erwartete Map an dieser Stelle
-     *
-     *                          [1,5,2013]
-     *                               |
-     *        _______________________|__________________
-     *       |                                          |
-     *       |                                          |
-     *   [2,4,2013]                                [6,12,2013]
-     *       |                                          |
-     *       |___________                    ___________|
-     *                   |                  |
-     *                   |                  |
-     *               [6,4,2013]         [5,5,2013]
-     *                   |
-     *             ______|
-     *            |
-     *            |
-     *        [5,4,2013]
-     */
+    cout << "                 Unsere Map an dieser Stelle             " << endl;
+    cout << "                                                         " << endl;
+    cout << "                          [1,5,2013]                     " << endl;
+    cout << "                               |                         " << endl;
+    cout << "        _______________________|__________________       " << endl;
+    cout << "       |                                          |      " << endl;
+    cout << "       |                                          |      " << endl;
+    cout << "   [2,4,2013]                                [6,12,2013] " << endl;
+    cout << "       |                                          |      " << endl;
+    cout << "       |___________                    ___________|      " << endl;
+    cout << "                   |                  |                  " << endl;
+    cout << "                   |                  |                  " << endl;
+    cout << "               [6,4,2013]         [5,5,2013]             " << endl;
+    cout << "                   |                                     " << endl;
+    cout << "             ______|______                               " << endl;
+    cout << "            |             |                              " << endl;
+    cout << "            |             |                              " << endl;
+    cout << "        [5,4,2013]    [7,4,2013]                         " << endl;
+    cout << "                                                         " << endl;
 
+    // Abfrage der Position der Daten in der map
     assert(map1.m_root->m_pair.first == date1);
     assert(map1.m_root->m_left->m_pair.first == date3);
     assert(map1.m_root->m_right->m_pair.first == date2);
     assert(map1.m_root->m_left->m_right->m_pair.first == date4);
     assert(map1.m_root->m_right->m_left->m_pair.first == date5);
-    assert(map1.m_root->m_left->m_right->m_left->m_pair.first == date6);
+    assert(map1.m_root->m_left->m_right->m_left->m_pair.first == date6);    
+    assert(map1.m_root->m_left->m_right->m_right->m_pair.first == date7);
 
 
 #if 0
