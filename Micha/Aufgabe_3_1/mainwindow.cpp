758c2cae755a003670a2884ffdf0d9231e6cefd9
Hasenkrug <togomade@gmail.com>
2013-06-22 16:52:19 +0200

    warmup ohne save

--- Micha/Aufgabe_3_1/mainwindow.cpp
+++ Micha/Aufgabe_3_1/mainwindow.cpp
@@ -1,14 +1,48 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "iostream"
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+
+    ui(new Ui::MainWindow) {
+        ui->setupUi(this);
+
+        connect(ui->inputField, SIGNAL(textChanged(QString)), this, SLOT(getSearchText(QString)));
+    }
+
+MainWindow::~MainWindow() {
+    delete ui;
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+void MainWindow::getSearchText(QString value) {
+
+    m_searchText = value;
+
+    cout << m_searchText.toStdString() << endl;
+}
+
+void MainWindow::on_btnQuit_clicked() {
+
+    QWidget *dialog = new QWidget();
+    dialog->setWindowTitle("Quit and Save");
+
+    QPushButton *cancel = new QPushButton("Abbruch");
+    QPushButton *save = new QPushButton("Speichern");
+    QPushButton *quit = new QPushButton("Applikation beenden");
+
+    QObject::connect(cancel, SIGNAL(clicked()), dialog, SLOT(close()));
+
+    QObject::connect(quit, SIGNAL(clicked()), this, SLOT(close()));
+    QObject::connect(quit, SIGNAL(clicked()), dialog, SLOT(close()));
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(cancel);
+    layout->addWidget(save);
+    layout->addWidget(quit);
+
+    dialog->setLayout(layout);
+    dialog->show();
 }
--- Micha/Aufgabe_3_1/mainwindow.h
+++ Micha/Aufgabe_3_1/mainwindow.h
@@ -4,19 +4,24 @@
 #include <QMainWindow>
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
     
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+public slots:
+    void getSearchText(QString value);
+    void on_btnQuit_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    QString m_searchText;
 };
 
 #endif // MAINWINDOW_H
